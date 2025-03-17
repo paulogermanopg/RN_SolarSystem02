ecca2652072c0cc7707ebaa05469b0ebad06789c
paulogermanopg <paulogermano.person@outlook.com.br>
2025-03-17 15:27:12 -0300

    mars: fobos and deimos

--- src/components/Mars.tsx
+++ src/components/Mars.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Dimensions } from 'react-native';
 import { Canvas, Circle, Group, LinearGradient, vec, Path } from '@shopify/react-native-skia';
+import { useSharedValue, useDerivedValue, withRepeat, withTiming, Easing } from 'react-native-reanimated';
 
 const { height, width } = Dimensions.get('window');
 
 const Mars = () => {
+  const fobosOrbit = useSharedValue(0);
+  const deimosOrbit = useSharedValue(0);
+
+  useEffect(() => {
+    fobosOrbit.value = withRepeat(
+      withTiming(2 * Math.PI, { duration: 4000, easing: Easing.linear }),
+      -1
+    );
+    deimosOrbit.value = withRepeat(
+      withTiming(2 * Math.PI, { duration: 7000, easing: Easing.linear }),
+      -1
+    );
+  }, [fobosOrbit, deimosOrbit]);
+
+  const fobosX = useDerivedValue(() => width / 2 + 50 * Math.cos(fobosOrbit.value));
+  const fobosY = useDerivedValue(() => height / 2 + 25 * Math.sin(fobosOrbit.value));
+
+  const deimosX = useDerivedValue(() => width / 2 + 70 * Math.cos(deimosOrbit.value));
+  const deimosY = useDerivedValue(() => height / 2 + 40 * Math.sin(deimosOrbit.value));
+
   return (
     <Canvas style={{ width, height }}>
       <Group>
@@ -37,8 +58,15 @@ const Mars = () => {
           color="#D8D8D8B3"
         />
       </Group>
+
+      {/* Luas de Marte */}
+      <Group>
+        <Circle cx={fobosX} cy={fobosY} r={5} color="#A9A9A9" />
+        <Circle cx={deimosX} cy={deimosY} r={4} color="#a27142" />
+      </Group>
     </Canvas>
   );
 };
 
 export default Mars;
+
